c75fb68a14d9037343ee3342bfc844ada45e3f75
SatyaDewangan05 <satyadewangan05@gmail.com>
2024-08-29 13:44:21 +0530

    Second commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "src/MultiAudioPlayer.js",
+  input: "src/index.js",
   output: [
     {
       file: "dist/index.js",
@@ -18,8 +20,11 @@ export default {
   ],
   plugins: [
     peerDepsExternal(),
+    resolve(),
+    commonjs(),
     babel({
       exclude: "node_modules/**",
+      babelHelpers: "bundled",
       presets: ["@babel/preset-env", "@babel/preset-react"],
     }),
     terser(),
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import MultiAudioVideoPlayer from "./MultiAudioVideoPlayer.jsx";
+
+export default MultiAudioVideoPlayer;
